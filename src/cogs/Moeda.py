ca8353ed1b2ffd33c9ed656c8f3ddd572f29fffb
Matheus Buratti <matheusburatti@alunos.utfpr.edu.br>
2021-04-16 15:32:32 -0300

    comando moeda/validar

--- src/cogs/Moeda.py
+++ src/cogs/Moeda.py
@@ -14,11 +14,33 @@ class Moeda(commands.Cog):
     # Recebe uma string com o nome da moeda e retorna a forma para ela ser usada nos comandos da api
     # Ex: recebe bitcoin e retorna BTC
     def validarComando(self, comando:str) -> str:
-        pass
+        if(comando.lower() in ("bitcoin","btc")):
+            return "btc"
+        elif(comando.lower() in ("dogecoin","doge")):
+            return "doge"
+        elif(comando.lower() in ("ethereum","eth")):
+            return "eth"
+        elif(comando.lower() in ("xrp")):
+            return "xrp"
+        elif(comando.lower() in ("bitcoin cash","bitcoincash","bch","btch")):
+            return "bch"
+        elif(comando.lower() in ("litecoin","ltc")):
+            return "ltc"
+        elif(comando.lower() in ("binance coin","binancecoin","bnb")):
+            return "bnb"
+        elif(comando.lower() in ("ethereum classic","ethereumclassic","etc")):
+            return "etc"
+        elif(comando.lower() in ("busd")):
+            return "busd"
+        elif(comando.lower() in ("tron","trx")):
+            return "trx"
+        return comando
+        
 
     @commands.command(aliases=["coin","preço","preco"])
     async def moeda(self,ctx,argument):
-        await ctx.send(cryptocompare.get_price('BTC', currency=['BRL','USD']))
+        moeda = self.validarComando(argument)
+        await ctx.send(cryptocompare.get_price(moeda, currency=['BRL','USD']))
 
 
 def setup(bot):
